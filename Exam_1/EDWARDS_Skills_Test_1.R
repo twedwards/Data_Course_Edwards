df17704915dd63de96be63e41cf9dec75db48e0a
Tyler Edwards <Tyler@Tylers-Macbook-Air.local>
2019-09-19 10:42:29 -0600

    Newest changes to Exam 1

--- Exam_1/EDWARDS_Skills_Test_1.R
+++ Exam_1/EDWARDS_Skills_Test_1.R
@@ -24,6 +24,38 @@ ggplot(DNA, aes(x=as.factor(DNA$Year_Collected), y=DNA$DNA_Concentration_Ben)) +
   theme(plot.title = element_text(hjust = .5))
 ggsave("EDWARDS_Plot2.jpg")
 
+summary(DNA$DNA_Concentration_Katy)
+summary(DNA$DNA_Concentration_Ben)
+
+Katy_mean=c()
+x=1
+for(i in unique(DNA$Year_Collected)){
+  Katy_mean[x] = mean(DNA[DNA$Year_Collected == i, "DNA_Concentration_Katy"])
+  x=x+1
+}
+
+Ben_mean=c()
+x1=1
+for(i in unique(DNA$Year_Collected)){
+  Ben_mean[x1] = mean(DNA[DNA$Year_Collected == i, "DNA_Concentration_Ben"])
+  x1=x1+1
+}
+
+plot(Ben_mean, col="red", yaxt='n', ann=FALSE) 
+par(new=TRUE)
+plot(Katy_mean, col="green", yaxt='n', ylab="Ben and Katy's Data collections", xlab="Year")
+
+
+downstairs <- DNA[DNA$Lab == "Downstairs",]
+
+ggplot(downstairs, aes(x=as.POSIXct(downstairs$Date_Collected), y=downstairs$DNA_Concentration_Ben)) +
+  geom_point() +
+  labs(x="Date Collected", y="Ben's Extractions", title = "Ben's DNA Extractions by Year") +
+  theme(plot.title = element_text(hjust = .5))
+ggsave("Ben_DNA_Over_Time.jpg")
+
+
+  
 
 
 
