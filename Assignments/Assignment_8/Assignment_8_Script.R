588b2e49d2e21e22f8bc88ce87d919e72fb48624
Tyler Edwards <Tyler@Tylers-Macbook-Air.local>
2019-10-21 15:53:49 -0600

    completed homwork

--- Assignments/Assignment_8/Assignment_8_Script.R
+++ Assignments/Assignment_8/Assignment_8_Script.R
@@ -42,33 +42,41 @@ mod5 <- lm(data = shrooms, GrowthRate ~ Light + Humidity)
 summary(mod5)
 mse(mod5)
 
+mse(mod1) #7697.293
+mse(mod2) #9631.201
+mse(mod3) #7605.163
+mse(mod4) #5525.948
+mse(mod5) #5828.882
+
+## mod4 is the best model
 
 
-df1 <- add_predictions(data = shrooms, model = mod1)
+
+df1 <- add_predictions(data = shrooms, model = mod3)
 df2 <- add_predictions(data = shrooms, model = mod4)
 df3 <- add_predictions(data = shrooms, model = mod5)
 
-ggplot(df1, aes(x=Nitrogen, y=GrowthRate)) +
-  geom_point() + geom_smooth(method = "lm")
+ggplot(shrooms, aes(x=Nitrogen, y=GrowthRate)) +
+  geom_point() + geom_point(aes(y = df1$pred), color = "Red", alpha = .25) + geom_smooth(method = "lm")
 
 ggplot(shrooms, aes(x=as.factor(Light), y=GrowthRate)) +
-  geom_boxplot() + facet_wrap(~Humidity)
+  geom_point() + geom_point(aes(y=df2$pred), color = "Red", alpha = .25) + facet_wrap(~Humidity) +
+  geom_smooth(method = "lm")
 
-ggplot(df2, aes(x=as.factor(Light), y=GrowthRate)) +
-  geom_point() + facet_wrap(~Humidity) + geom_point(aes(y=pred), color = "Red")
+ggplot(shrooms, aes(x=as.factor(Light), y=GrowthRate)) +
+  geom_point() + geom_point(aes(y=df3$pred), color = "Red", alpha = .25)+
+  geom_smooth(method = "lm")
 
 
-ggplot(df3, aes(x=as.factor(Light), y=GrowthRate)) +
-  geom_point() + facet_wrap(~Humidity) + geom_point(aes(y=pred), color = "Red") +
+ggplot(shrooms, aes(x=as.factor(Light), y=GrowthRate)) +
+  geom_point() + facet_wrap(~Humidity) + geom_point(aes(y=df2$pred), color = "Red") +
   labs(title = "Growth Rate as a Function of Light and Humidity", y="Growth Rate", x="Light"
        , caption = "Note: Red points are plotted predictions")
 
 
-mean(abs(residuals(mod1)))
-mean(abs(residuals(mod2)))
-mean(abs(residuals(mod3)))
-mean(abs(residuals(mod4)))
-mean(abs(residuals(mod5)))
+#1. Mod 2 and 3 were both scientifically insignificant. They are insignificant because they're p-value was far above .05
+#2. All were linear relationships
+
 
 
 
