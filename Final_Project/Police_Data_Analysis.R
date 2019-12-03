78c1b079ff01b81270ab79df24909891caafb886
twedwards <10782879@my.uvu.edu>
2019-12-02 17:27:24 -0700

    new changes to final project

--- Final_Project/Police_Data_Analysis.R
+++ Final_Project/Police_Data_Analysis.R
@@ -1,22 +1,24 @@
 library(tidyverse)
 library(modelr)
 
-
+install.packages("devtools")
+devtools::install_github("username/packagename")
 
 clean <- read.csv("./Cleaned_Police_Data.csv")
 
-df1 <- clean[1:5000,]
+mod1 <- glm(data=clean, VehicleSearchConducted ~ DriverRace + Age, family="binomial")
+clean <- add_predictions(clean,mod1,type="response",var="ProbOfSearchmod1")
 
-mod1 <- glm(data=df1, VehicleSearchConducted ~ DriverRace + Age, family="binomial")
-df1 <- add_predictions(df1,mod1,type="response",var="ProbOfSearchmod1")
+mod2 <- glm(data=clean, VehicleSearchConducted ~ DriverRace + AgencyCode, family="binomial")
+clean <- add_predictions(clean, mod2, type="response",var="ProbOfSearchmod2")
 
-mod2 <- aov(data=df1, VehicleSearchConducted ~ DriverRace + AgencyName)
-df1 <- add_predictions(df1, mod2, type="response",var="ProbOfSearchmod2")
+mod3 <- aov(data=clean, VehicleSearchConducted ~ DriverRace + ZIP)
+clean <- add_predictions(data=clean,mod3, type="response", var="ProbOfSearchmod3")
 
-mod3 <- aov(data=clean, VehicleSearchConducted ~ DriverRace + AgencyName)
+unique(clean$ZIP)
+unique(clean$DriverRace)
 
-summary(mod1)
-summary(mod2)
+
 
 
 ggplot(clean, aes(x=ProbOfSearchmod1)) +
@@ -24,13 +26,17 @@ ggplot(clean, aes(x=ProbOfSearchmod1)) +
 
 
 
-mod3 <- glm(data = clean, VehicleSearchConducted ~ AgencyName)
-clean <- add_predictions(data = clean,mod3, type="response",var = "SearchByAgency")
+plot1 <- ggplot(clean, aes(x=Age, y=ProbOfSearchmod1)) +
+  geom_point() + facet_wrap(~DriverRace)
+
+plot2 <- ggplot(clean, aes(x=Age, y=ProbOfSearchmod2)) +
+  geom_point() + facet_wrap(~DriverRace)
+
+plot2
 
 
 
-ggplot(clean, aes(x=ProbOfContraFound, y=Age, color=DriverRace)) +
-  geom_point()
+
 summary(mod1)
 summary(mod2)
 summary(mod3)
