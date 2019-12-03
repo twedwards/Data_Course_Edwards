aa78fe2c624abf12b5c8409a64dbd6d388c75fed
Tyler <10782879@my.uvu.edu>
2019-12-02 23:30:46 -0700

    New changes to final project

--- Final_Project/Police_Data_Analysis.R
+++ Final_Project/Police_Data_Analysis.R
@@ -1,9 +1,6 @@
 library(tidyverse)
 library(modelr)
 
-install.packages("devtools")
-devtools::install_github("username/packagename")
-
 clean <- read.csv("./Cleaned_Police_Data.csv")
 
 mod1 <- glm(data=clean, VehicleSearchConducted ~ DriverRace + Age, family="binomial")
@@ -12,14 +9,8 @@ clean <- add_predictions(clean,mod1,type="response",var="ProbOfSearchmod1")
 mod2 <- glm(data=clean, VehicleSearchConducted ~ DriverRace + AgencyCode, family="binomial")
 clean <- add_predictions(clean, mod2, type="response",var="ProbOfSearchmod2")
 
-mod3 <- aov(data=clean, VehicleSearchConducted ~ DriverRace + ZIP)
+mod3 <- lm(data=clean, VehicleContrabandFound ~ DriverRace)
 clean <- add_predictions(data=clean,mod3, type="response", var="ProbOfSearchmod3")
-
-unique(clean$ZIP)
-unique(clean$DriverRace)
-
-
-
 
 ggplot(clean, aes(x=ProbOfSearchmod1)) +
   geom_histogram(aes(fill=clean$DriverRace))
@@ -34,6 +25,9 @@ plot2 <- ggplot(clean, aes(x=Age, y=ProbOfSearchmod2)) +
 
 plot2
 
+ggplot(clean, aes(x=Age, y=ProbOfSearchmod3))+
+  geom_point() + facet_wrap(~DriverRace)
+unique(clean$VehicleContrabandFound)
 
 
 
